00141644f836092cb90b51479dea11b0c10219f3
neelp14 <69315341+neelp14@users.noreply.github.com>
2021-10-07 16:41:58 -0400

    Update run_tfagnet.sh
    Corrected the mounting directory issues

--- run_tfagnet.sh
+++ run_tfagnet.sh
@@ -1,2 +1,2 @@
-docker run -v /home/nxp225/:/mnt/ -t docker.io/bushlab/tfagnet:latest python3 /TFAGNet/code/run_tfagnet.py -v /mnt/$1 -b $2 -o $3 
-docker run -v /home/nxp225/:/mnt/ -t docker.io/bushlab/tfagnet:latest chmod 777 -R /mnt/$3_results/ 
+docker run -v $1:/mnt/ -t docker.io/bushlab/tfagnet:latest python3 /TFAGNet/code/run_tfagnet.py -v /mnt/$2 -b $3 -o $4 
+docker run -v $1:/mnt/ -t docker.io/bushlab/tfagnet:latest chmod 777 -R /mnt/$4_results/ 
