6aceabcde06225083cc7a0ecf3acee7ab6723a9d
neelp14 <69315341+neelp14@users.noreply.github.com>
2021-10-08 13:35:23 -0400

    Update run_tfagnet_custom_tfbs.sh

--- run_tfagnet_custom_tfbs.sh
+++ run_tfagnet_custom_tfbs.sh
@@ -1,2 +1,8 @@
-docker run -v $PWD:/mnt/ -t docker.io/bushlab/tfagnet:latest python3 /TFAGNet/code/run_tfagnet.py -v /mnt/$1 -t /mnt/$2 -b $3 -o $4 
-docker run -v $PWD:/mnt/ -t docker.io/bushlab/tfagnet:latest chmod 777 -R /mnt/$4_results/ 
+if [ $1 == "docker" ]
+then
+        docker run -v $PWD:/mnt/ -t docker.io/bushlab/tfagnet:latest python3 /TFAGNet/code/run_tfagnet.py -v /mnt/$2 -t /mnt/$3 -b $4 -o $5
+        docker run -v $PWD:/mnt/ -t docker.io/bushlab/tfagnet:latest chmod 777 -R /mnt/$5_results/
+else
+        singularity exec --bind $PWD:/mnt/ tfagnet_latest.sif python3 /TFAGNet/code/run_tfagnet.py -v /mnt/$2 -t /mnt/$3 -b $4 -o $5
+
+fi
