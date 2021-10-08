6392a2187060fd604c9acbe9a539bef1ce8df107
neelp14 <69315341+neelp14@users.noreply.github.com>
2021-10-08 13:31:55 -0400

    Mounting  and singularity issues corrected

--- run_tfagnet.sh
+++ run_tfagnet.sh
@@ -1,2 +1,10 @@
-docker run -v $PWD:/mnt/ -t docker.io/bushlab/tfagnet:latest python3 /TFAGNet/code/run_tfagnet.py -v /mnt/$1 -b $2 -o $3 
-docker run -v $PWD:/mnt/ -t docker.io/bushlab/tfagnet:latest chmod 777 -R /mnt/$3_results/ 
+if [ $1 == "docker" ]
+then
+        docker run -v $PWD:/mnt/ -t docker.io/bushlab/tfagnet:latest python3 /TFAGNet/code/run_tfagnet.py -v /mnt/$2 -b $3 -o $4
+        docker run -v $PWD:/mnt/ -t docker.io/bushlab/tfagnet:latest chmod 777 -R /mnt/$4_results/
+else
+        singularity exec --bind $PWD:/mnt/ tfagnet_latest.sif python3 /TFAGNet/code/run_tfagnet.py -v /mnt/$2 -b $3 -o $4
+
+fi
+
+
